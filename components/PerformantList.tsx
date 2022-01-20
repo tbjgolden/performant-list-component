ed00519973ca5600c55a85be99d05957fd182db2
Tom <8162045+tbjgolden@users.noreply.github.com>
2022-01-20 11:10:15 +0000

    cleaned up and added checkboxes

--- components/PerformantList.tsx
+++ components/PerformantList.tsx
@@ -1,39 +1,55 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState } from "react";
+import styles from "./PerformantList.module.css";
 
-type JSONObject = { [key: string]: JSONValue } | JSONValue[]
-type JSONValue = string | number | boolean | JSONObject
+type JSONObject = { [key: string]: JSONValue } | JSONValue[];
+type JSONValue = string | number | boolean | JSONObject;
 
 export const PerformantList = <T extends JSONObject, U = number | string>({
   items,
-  renderItem
+  renderItem,
 }: {
   items: {
-    uid: U,
-    value: T
-  }[],
-  renderItem: (item: T, index: number) => JSX.Element
+    uid: U;
+    value: T;
+  }[];
+  renderItem: (item: T, index: number) => JSX.Element;
 }): JSX.Element => {
   // selected is stored as sorted JSON array of uids
-  const [selectedList, setSelectedList] = useState("[]")
-  const selectedSet = useMemo(() => new Set<U>(JSON.parse(selectedList)), [selectedList])
+  const [selectedList, setSelectedList] = useState("[]");
+  const selectedSet = useMemo(
+    () => new Set<U>(JSON.parse(selectedList)),
+    [selectedList]
+  );
   const toggleUID = (uid: U) => {
     if (selectedSet.has(uid)) {
-      selectedSet.delete(uid)
+      selectedSet.delete(uid);
     } else {
-      selectedSet.add(uid)
+      selectedSet.add(uid);
     }
-    setSelectedList(JSON.stringify([...selectedSet.values()].sort()))
-  }
+    setSelectedList(JSON.stringify([...selectedSet.values()].sort()));
+  };
 
-  return <div>
-    Selected: {selectedList}
-    <hr />
-    {
-      items.map((item, index) => (
-        <div key={item.uid + ""}>
-          {renderItem(item.value, index)}
+  return (
+    <div>
+      Selected: {selectedList}
+      <hr />
+      {items.map((item, index) => (
+        <div className={styles.listItem} key={item.uid + ""}>
+          <div className={styles.listItemCheckboxWrapper}>
+            <input
+              type="checkbox"
+              name="checked-items"
+              checked={selectedSet.has(item.uid)}
+              onChange={() => {
+                toggleUID(item.uid);
+              }}
+            />
+          </div>
+          <div className={styles.listItemContentWrapper}>
+            {renderItem(item.value, index)}
+          </div>
         </div>
-      ))
-    }
-  </div>
-}
+      ))}
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { PerformantList } from 'components/PerformantList'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { PerformantList } from "components/PerformantList";
+import styles from "../styles/Home.module.css";
 
-const DJ_KHALED_SOURCE = `Watch your back, but more importantly when you get out the shower, dry your back, it's a cold world out there. Congratulations, you played yourself. Look at the sunset, life is amazing, life is beautiful, life is what you make it. Bless up. Every chance I get, I water the plants, Lion! Egg whites, turkey sausage, wheat toast, water. Of course they don't want us to eat our breakfast, so we are going to enjoy our breakfast. Another one. Eliptical talk. Congratulations, you played yourself. Stay focused. Lion! Congratulations, you played yourself. I'm giving you cloth talk, cloth. Special cloth alert, cut from a special cloth. Mogul talk. Watch your back, but more importantly when you get out the shower, dry your back, it's a cold world out there. The other day the grass was brown, now it's green because I ain't give up. Never surrender. They don't want us to eat. Life is what you make it, so let's make it. I told you all this before, when you have a swimming pool, do not use chlorine, use salt water, the healing, salt water is the healing. To be successful you've got to work hard, to make history, simple, you've got to make it. Let's see what Chef Dee got that they don't want us to eat. Life is what you make it, so let's make it. We don't see them, we will never see them. Find peace, life is like a water fall, you've gotta flow. The key to more success is to have a lot of pillows. How's business? Boomin. Surround yourself with angels. The other day the grass was brown, now it's green because I ain't give up. Never surrender. Lion! Don't ever play yourself. The key to more success is to get a massage once a week, very important, major key, cloth talk. Surround yourself with angels, positive energy, beautiful people, beautiful souls, clean heart, angel. Another one. Watch your back, but more importantly when you get out the shower, dry your back, it's a cold world out there. A major key, never panic. Don't panic, when it gets crazy and rough, don't panic, stay calm. It's important to use cocoa butter. It's the key to more success, why not live smooth? Why live rough? To be successful you've got to work hard, to make history, simple, you've got to make it. Life is what you make it, so let's make it. Lion! Learning is cool, but knowing is better, and I know the key to success. Every chance I get, I water the plants, Lion! Learning is cool, but knowing is better, and I know the key to success. Don't ever play yourself. I'm up to something. The ladies always say Khaled you smell good, I use no cologne. Cocoa butter is the key. Always remember in the jungle there's a lot of they in there, after you overcome they, you will make it to paradise. Mogul talk. Eliptical talk. Give thanks to the most high. We the best. They don't want us to win. Cloth talk. We the best. You see the hedges, how I got it shaped up? It's important to shape up your hedges, it's like getting a haircut, stay fresh. Another one. Celebrate success right, the only way, apple. Give thanks to the most high. Give thanks to the most high. They key is to have every key, the key to open every door. Fan luv. Congratulations, you played yourself. You see the hedges, how I got it shaped up? It's important to shape up your hedges, it's like getting a haircut, stay fresh. Watch your back, but more importantly when you get out the shower, dry your back, it's a cold world out there. Celebrate success right, the only way, apple. The weather is amazing, walk with me through the pathway of more success. Take this journey with me, Lion! Mogul talk. To be successful you've got to work hard, to make history, simple, you've got to make it. Give thanks to the most high. They never said winning was easy. Some people can't handle success, I can. Surround yourself with angels, positive energy, beautiful people, beautiful souls, clean heart, angel. Eliptical talk. To be successful you've got to work hard, to make history, simple, you've got to make it. In life you have to take the trash out, if you have trash in your life, take it out, throw it away, get rid of it, major key. Learning is cool, but knowing is better, and I know the key to success. Surround yourself with angels, positive energy, beautiful people, beautiful souls, clean heart, angel. I told you all this before, when you have a swimming pool, do not use chlorine, use salt water, the healing, salt water is the healing. Major key, don't fall for the trap, stay focused. It's the ones closest to you that want to see you fail. I told you all this before, when you have a swimming pool, do not use chlorine, use salt water, the healing, salt water is the healing. The weather is amazing, walk with me through the pathway of more success. Take this journey with me, Lion! Life is what you make it, so let's make it. We the best. I'm giving you cloth talk, cloth. Special cloth alert, cut from a special cloth. Find peace, life is like a water fall, you've gotta flow. Watch your back, but more importantly when you get out the shower, dry your back, it's a cold world out there. A major key, never panic. Don't panic, when it gets crazy and rough, don't panic, stay calm`
-const DJ_KHALED_SENTENCES = DJ_KHALED_SOURCE.split(". ").map(s => s + ".")
+const DJ_KHALED_SOURCE = `Watch your back, but more importantly when you get out the shower, dry your back, it's a cold world out there. Congratulations, you played yourself. Look at the sunset, life is amazing, life is beautiful, life is what you make it. Bless up. Every chance I get, I water the plants, Lion! Egg whites, turkey sausage, wheat toast, water. Of course they don't want us to eat our breakfast, so we are going to enjoy our breakfast. Another one. Eliptical talk. Congratulations, you played yourself. Stay focused. Lion! Congratulations, you played yourself. I'm giving you cloth talk, cloth. Special cloth alert, cut from a special cloth. Mogul talk. Watch your back, but more importantly when you get out the shower, dry your back, it's a cold world out there. The other day the grass was brown, now it's green because I ain't give up. Never surrender. They don't want us to eat. Life is what you make it, so let's make it. I told you all this before, when you have a swimming pool, do not use chlorine, use salt water, the healing, salt water is the healing. To be successful you've got to work hard, to make history, simple, you've got to make it. Let's see what Chef Dee got that they don't want us to eat. Life is what you make it, so let's make it. We don't see them, we will never see them. Find peace, life is like a water fall, you've gotta flow. The key to more success is to have a lot of pillows. How's business? Boomin. Surround yourself with angels. The other day the grass was brown, now it's green because I ain't give up. Never surrender. Lion! Don't ever play yourself. The key to more success is to get a massage once a week, very important, major key, cloth talk. Surround yourself with angels, positive energy, beautiful people, beautiful souls, clean heart, angel. Another one. Watch your back, but more importantly when you get out the shower, dry your back, it's a cold world out there. A major key, never panic. Don't panic, when it gets crazy and rough, don't panic, stay calm. It's important to use cocoa butter. It's the key to more success, why not live smooth? Why live rough? To be successful you've got to work hard, to make history, simple, you've got to make it. Life is what you make it, so let's make it. Lion! Learning is cool, but knowing is better, and I know the key to success. Every chance I get, I water the plants, Lion! Learning is cool, but knowing is better, and I know the key to success. Don't ever play yourself. I'm up to something. The ladies always say Khaled you smell good, I use no cologne. Cocoa butter is the key. Always remember in the jungle there's a lot of they in there, after you overcome they, you will make it to paradise. Mogul talk. Eliptical talk. Give thanks to the most high. We the best. They don't want us to win. Cloth talk. We the best. You see the hedges, how I got it shaped up? It's important to shape up your hedges, it's like getting a haircut, stay fresh. Another one. Celebrate success right, the only way, apple. Give thanks to the most high. Give thanks to the most high. They key is to have every key, the key to open every door. Fan luv. Congratulations, you played yourself. You see the hedges, how I got it shaped up? It's important to shape up your hedges, it's like getting a haircut, stay fresh. Watch your back, but more importantly when you get out the shower, dry your back, it's a cold world out there. Celebrate success right, the only way, apple. The weather is amazing, walk with me through the pathway of more success. Take this journey with me, Lion! Mogul talk. To be successful you've got to work hard, to make history, simple, you've got to make it. Give thanks to the most high. They never said winning was easy. Some people can't handle success, I can. Surround yourself with angels, positive energy, beautiful people, beautiful souls, clean heart, angel. Eliptical talk. To be successful you've got to work hard, to make history, simple, you've got to make it. In life you have to take the trash out, if you have trash in your life, take it out, throw it away, get rid of it, major key. Learning is cool, but knowing is better, and I know the key to success. Surround yourself with angels, positive energy, beautiful people, beautiful souls, clean heart, angel. I told you all this before, when you have a swimming pool, do not use chlorine, use salt water, the healing, salt water is the healing. Major key, don't fall for the trap, stay focused. It's the ones closest to you that want to see you fail. I told you all this before, when you have a swimming pool, do not use chlorine, use salt water, the healing, salt water is the healing. The weather is amazing, walk with me through the pathway of more success. Take this journey with me, Lion! Life is what you make it, so let's make it. We the best. I'm giving you cloth talk, cloth. Special cloth alert, cut from a special cloth. Find peace, life is like a water fall, you've gotta flow. Watch your back, but more importantly when you get out the shower, dry your back, it's a cold world out there. A major key, never panic. Don't panic, when it gets crazy and rough, don't panic, stay calm`;
+const DJ_KHALED_SENTENCES = DJ_KHALED_SOURCE.split(". ").map((s) => s + ".");
 
 const TEST_ITEMS = new Array(1000).fill(0).map((_, i) => {
-  const uid = 10000 + i
-  const sentence = DJ_KHALED_SENTENCES[uid % DJ_KHALED_SENTENCES.length]
-  return ({
+  const uid = 10000 + i;
+  const sentence = DJ_KHALED_SENTENCES[uid % DJ_KHALED_SENTENCES.length];
+  return {
     uid,
     value: {
       uid,
-      sentence
-    }
-  });
-})
+      sentence,
+    },
+  };
+});
 
 const Home: NextPage = () => {
   return (
@@ -26,23 +26,23 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Performant List Component
-        </h1>
+        <h1 className={styles.title}>Performant List Component</h1>
 
         <div className={styles.listWrapper}>
           <PerformantList
             items={TEST_ITEMS}
             renderItem={(item, index) => {
-              return <div style={{ border: '2px solid #ccc', padding: 8 }}>
-                Item({index}): {item.sentence}
-              </div>
+              return (
+                <div style={{ border: "2px solid #ccc", padding: 8 }}>
+                  Item({index}): {item.sentence}
+                </div>
+              );
             }}
           />
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
